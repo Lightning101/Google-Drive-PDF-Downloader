f624e3053f038834f6062e55b5d18b6a3cb54b14
Dhinendra Rajapakse <dhinendra@simct.com>
2021-07-16 10:51:37 +0530

    * Fixed issues in code. * Updated readme

--- Method_1_Script.js
+++ Method_1_Script.js
@@ -1,93 +1,97 @@
-function generatePDFDataFile(pdfDocumentName) {
-    let doc = "";
-    let imgTags = document.getElementsByTagName("img");
-    let checkURLString = "blob:https://drive.google.com/";
-    let validImgTagCounter = 0;
-    for (i = 0; i < imgTags.length; i++) {
 
-        if (imgTags[i].src.substring(0, checkURLString.length) === checkURLString) {
-            validImgTagCounter = validImgTagCounter + 1;
-            //console.log(imgTags[i].src);
-            let img = imgTags[i];
+class GooglePDFGenerator {
+    static generatePDFDataFile(pdfDocumentName) {
+        let doc = "";
+        let imgTags = document.getElementsByTagName("img");
+        let checkURLString = "blob:https://drive.google.com/";
+        let validImgTagCounter = 0;
+        for (let i = 0; i < imgTags.length; i++) {
 
-            let canvas = document.createElement('canvas');
-            let context = canvas.getContext("2d");
-            canvas.width = img.naturalWidth;
-            canvas.height = img.naturalHeight;
-            //console.log("Width: " + img.naturalWidth + ", Height: " + img.naturalHeight);
-            context.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
-            let imgDataURL = canvas.toDataURL();
-            // console.log(imgDataURL);
+            if (imgTags[i].src.substring(0, checkURLString.length) === checkURLString) {
+                validImgTagCounter = validImgTagCounter + 1;
+                //console.log(imgTags[i].src);
+                let img = imgTags[i];
 
-            if (doc === "") {
-                doc = imgDataURL;
-            } else {
-                doc = doc + "\n" + imgDataURL;
+                let canvas = document.createElement('canvas');
+                let context = canvas.getContext("2d");
+                canvas.width = img.naturalWidth;
+                canvas.height = img.naturalHeight;
+                //console.log("Width: " + img.naturalWidth + ", Height: " + img.naturalHeight);
+                context.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+                let imgDataURL = canvas.toDataURL();
+                // console.log(imgDataURL);
+
+                if (doc === "") {
+                    doc = imgDataURL;
+                } else {
+                    doc = doc + "\n" + imgDataURL;
+                }
             }
         }
+
+        let anchorElement = document.createElement("a");
+        let file = new Blob([doc], { type: 'text/plain' });
+
+        const url = URL.createObjectURL(file);
+        anchorElement.href = url;
+        anchorElement.download = pdfDocumentName + ".PDF_DataFile";
+        document.body.appendChild(anchorElement);
+        console.log(anchorElement)
+        anchorElement.click();
     }
 
-    let anchorElement = document.createElement("a");
-    let file = new Blob([doc], { type: 'text/plain' });
+    static startPDFGenerate(documentName = "Document") {
+        let allElements = document.querySelectorAll("*");
+        let chosenElement;
+        let heightOfScrollableElement = 0;
 
-    url = URL.createObjectURL(file);
-    anchorElement.href = url;
-    anchorElement.download = pdfDocumentName + ".PDF_DataFile";
-    document.body.appendChild(anchorElement);
-    anchorElement.click();
-}
-
-function scrollToBottom(remainingHeightToScroll, scrollToLocation, scrollDistance, callback) {
-    setTimeout(function () {
-        if (remainingHeightToScroll === 0) {
-            scrollToLocation = scrollDistance;
-            chosenElement.scrollTo(0, scrollToLocation);
-            remainingHeightToScroll = chosenElement.scrollHeight - scrollDistance;
-        } else {
-            scrollToLocation = scrollToLocation + scrollDistance;
-            chosenElement.scrollTo(0, scrollToLocation);
-            remainingHeightToScroll = remainingHeightToScroll - scrollDistance;
+        for (let i = 0; i < allElements.length; i++) {
+            if (allElements[i].scrollHeight >= allElements[i].clientHeight) {
+                if (heightOfScrollableElement < allElements[i].scrollHeight) {
+                    //console.log(allElements[i]);
+                    //console.log(allElements[i].scrollHeight);
+                    heightOfScrollableElement = allElements[i].scrollHeight;
+                    chosenElement = allElements[i];
+                }
+            }
         }
 
-        if (remainingHeightToScroll >= chosenElement.clientHeight) {
-            scrollToBottom(remainingHeightToScroll, scrollToLocation, scrollDistance, callback)
+        if (chosenElement.scrollHeight > chosenElement.clientHeight) {
+            console.log("Auto Scroll");
+            let scrollDistance = Math.round(chosenElement.clientHeight / 2);
+            //console.log("scrollHeight: " + chosenElement.scrollHeight);
+            //console.log("scrollDistance: " + scrollDistance);
+
+            const scrollToBottom = (remainingHeightToScroll, scrollToLocation) => {
+                setTimeout(() => {
+                    if (remainingHeightToScroll === 0) {
+                        scrollToLocation = scrollDistance;
+                        chosenElement.scrollTo(0, scrollToLocation);
+                        remainingHeightToScroll = chosenElement.scrollHeight - scrollDistance;
+                    } else {
+                        scrollToLocation = scrollToLocation + scrollDistance;
+                        chosenElement.scrollTo(0, scrollToLocation);
+                        remainingHeightToScroll = remainingHeightToScroll - scrollDistance;
+                    }
+
+                    if (remainingHeightToScroll >= chosenElement.clientHeight) {
+                        scrollToBottom(remainingHeightToScroll, scrollToLocation)
+                    } else {
+                        setTimeout(() => {
+                            GooglePDFGenerator.generatePDFDataFile(documentName);
+                        }, 1500)
+                    }
+
+                }, 400)
+            }
+            scrollToBottom(0, 0);
+
         } else {
             setTimeout(function () {
-                callback();
+                GooglePDFGenerator.generatePDFDataFile(documentName);
             }, 1500)
         }
-
-    }, 400)
-}
-
-function startPDFGenerate(documentName = "Document") {
-    let allElements = document.querySelectorAll("*");
-    let chosenElement;
-    let heightOfScrollableElement = 0;
-
-    for (i = 0; i < allElements.length; i++) {
-        if (allElements[i].scrollHeight >= allElements[i].clientHeight) {
-            if (heightOfScrollableElement < allElements[i].scrollHeight) {
-                //console.log(allElements[i]);
-                //console.log(allElements[i].scrollHeight);
-                heightOfScrollableElement = allElements[i].scrollHeight;
-                chosenElement = allElements[i];
-            }
-        }
-    }
-
-    if (chosenElement.scrollHeight > chosenElement.clientHeight) {
-        console.log("Auto Scroll");
-        let scrollDistance = Math.round(chosenElement.clientHeight / 2);
-        //console.log("scrollHeight: " + chosenElement.scrollHeight);
-        //console.log("scrollDistance: " + scrollDistance);
-        scrollToBottom(0, 0, scrollDistance, generatePDF_DataFile);
-
-    } else {
-        console.log("No Scroll");
-        setTimeout(function () {
-            generatePDFDataFile();
-        }, 1500)
     }
 }
+
 
